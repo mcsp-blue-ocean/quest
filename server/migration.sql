1cf53c8caa844ff0eaa46d993236696f9fb6ae2f
Quinn Kaji <quinnkaji@gmail.com>
2023-12-15 11:41:08 -1000

    cleaned things up

--- server/migration.sql
+++ server/migration.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `commands`(
-    `id` INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` INT  NOT NULL  PRIMARY KEY,
     `category_id` INT NOT NULL,
     `command_syntax` TEXT NOT NULL,
     `command_description` TEXT NOT NULL
 );
 CREATE TABLE `categories`(
-    `id` INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` INT  NOT NULL  PRIMARY KEY,
     `category` TEXT NOT NULL
 );
 ALTER TABLE
