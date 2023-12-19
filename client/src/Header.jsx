b4982b28fa3c25b9b2c79259b159c76cad0aa417
Greg Pappas <gnpappas@proton.me>
2023-12-19 11:42:37 -1000

    Update Panel component in Header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,13 +1,13 @@
 import Navigation from "./Header/Navigation";
-import Icons from "./Header/Icons";
+import Panel from "./Header/Panel";
 import logo from "./assets/header/logo.png?web";
 
 const Header = () => {
   return (
-    <div className="flex sm:flex-row flex-col items-center justify-between p-3 bg-sky-800 text-slate-200 uppercase">
+    <div className="flex sm:flex-row flex-col items-center justify-between p-3 bg-sky-800 uppercase">
       <Navigation />
       <img src={logo} className="w-28 order-first sm:order-none" />
-      <Icons />
+      <Panel />
     </div>
   );
 };
