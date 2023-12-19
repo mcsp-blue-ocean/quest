03558fdede923f45aee76e7811afb4f42bb4576d
Greg Pappas <gnpappas@proton.me>
2023-12-19 05:30:28 -1000

    Add directive to optimize images to webp format

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import { imagetools } from "vite-imagetools";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    imagetools({
+      defaultDirectives: (url) => {
+        if (url.searchParams.has("web")) {
+          return new URLSearchParams("format=webp");
+        }
+        return new URLSearchParams();
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": `http://localhost:${process.env.PORT}`,
