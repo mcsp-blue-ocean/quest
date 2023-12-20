34226b823a43a19e513ac0d287f7262ef050fbea
Greg Pappas <gnpappas@proton.me>
2023-12-20 09:35:22 -1000

    Add alt tag

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="flex sm:flex-row flex-col items-center justify-between p-3 bg-sky-800 uppercase">
       <Navigation />
-      <img src={logo} className="w-28 order-first sm:order-none" />
+      <img src={logo} alt="logo" className="w-28 order-first sm:order-none" />
       <img src={login} alt="login" className="w-14" />
     </div>
   );
