1546278547aabe64a517caea41ecc7c81cc1be34
Greg Pappas <gnpappas@proton.me>
2023-12-19 15:32:55 -1000

    Update import statements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./Header";
-import How from "./How";
-import Example from "./Example";
+import How from "./Landing/How";
+import Example from "./Landing/Example";
 function App() {
   return (
     <div id="main" className="h-[100dvh] text-stone-200">
