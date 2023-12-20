df0017cc0d321c50f464180232e58904dd9a1811
Greg Pappas <gnpappas@proton.me>
2023-12-19 15:38:25 -1000

    Lading component to compartmentalize all children

---
+++ client/src/Landing.jsx
@@ -0,0 +1,13 @@
+import Example from "./Landing/Example";
+import How from "./Landing/How";
+
+const Landing = () => {
+  return (
+    <>
+      <How />
+      <Example />
+    </>
+  );
+};
+
+export default Landing;
