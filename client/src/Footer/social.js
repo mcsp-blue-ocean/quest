54da758e897f1bd3eb6007e3a53fefe5e17efcac
Greg Pappas <gnpappas@proton.me>
2023-12-20 09:34:32 -1000

    Add data for social icons

---
+++ client/src/Footer/social.js
@@ -0,0 +1,33 @@
+import discord from "../assets/footer/discord.png?web";
+import fb from "../assets/footer/fb.png?web";
+import li from "../assets/footer/li.png?web";
+import share from "../assets/footer/share.png?web";
+import x from "../assets/footer/x.png?web";
+
+export const socialIcons = [
+  {
+    id: 0,
+    icon: discord,
+    alt: "discord",
+  },
+  {
+    id: 1,
+    icon: fb,
+    alt: "facebook",
+  },
+  {
+    id: 2,
+    icon: li,
+    alt: "linked in",
+  },
+  {
+    id: 3,
+    icon: share,
+    alt: "share",
+  },
+  {
+    id: 4,
+    icon: x,
+    alt: "share",
+  },
+];
