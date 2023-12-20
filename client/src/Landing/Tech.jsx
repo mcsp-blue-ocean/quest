cb8e4a009002ff42e72ed76b5b8ad31b18814167
Greg Pappas <gnpappas@proton.me>
2023-12-20 11:35:06 -1000

    Move generated list into ul and put img into li.

--- client/src/Landing/Tech.jsx
+++ client/src/Landing/Tech.jsx
@@ -1,43 +1,18 @@
-import html from "../assets/how/html.png?web";
-import css from "../assets/how/css.png?web";
-import js from "../assets/how/js.png?web";
-import react from "../assets/how/react.png?web";
-import sql from "../assets/how/sql.png?web";
+import { techIcons } from "./techData";
 
 const Tech = () => {
-  const techIcons = [
-    {
-      id: 0,
-      image: html,
-    },
-    {
-      id: 1,
-      image: css,
-    },
-    {
-      id: 2,
-      image: js,
-    },
-    {
-      id: 3,
-      image: react,
-    },
-    {
-      id: 4,
-      image: sql,
-    },
-  ];
   return (
-    <div id="tech-icons" className="flex">
-      {techIcons.map(({ id, image }) => (
-        <img
-          key={id}
-          src={image}
-          alt={`icon ${id}`}
-          style={{ width: "100%", aspectRatio: "1/1" }}
-        ></img>
+    <ul id="tech-icons" className="flex">
+      {techIcons.map(({ id, image, alt }) => (
+        <li key={id}>
+          <img
+            src={image}
+            alt={alt}
+            style={{ width: "100%", aspectRatio: "1/1" }}
+          ></img>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
 
