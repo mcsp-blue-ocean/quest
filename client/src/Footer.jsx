c4f53c140f6660aa8b6573d6fd83e61aa12e77b8
Greg Pappas <gnpappas@proton.me>
2023-12-20 09:35:11 -1000

    Create Footer component

---
+++ client/src/Footer.jsx
@@ -0,0 +1,21 @@
+import Social from "./Footer/Social";
+
+const Footer = () => {
+  const navigation = ["Features", "Resources", "Blog", "Support"];
+  return (
+    <div className="flex flex-col items-center gap-10 px-20 py-10">
+      <ul className="flex gap-32 justify-center">
+        {navigation.map((link, index) => (
+          <li key={index}>{link}</li>
+        ))}
+      </ul>
+      <hr className="w-full" />
+      <Social />
+      <p className="font-bold">
+        © {new Date().getFullYear().toString()} QUEST
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
