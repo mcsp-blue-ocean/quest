bf087db62c8560e4c33965329fd8c57c5d42871e
Greg Pappas <gnpappas@proton.me>
2023-12-19 15:38:12 -1000

    Add Landing component to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import Header from "./Header";
-import How from "./Landing/How";
-import Example from "./Landing/Example";
+import Landing from "./Landing";
 function App() {
   return (
     <div id="main" className="h-[100dvh] text-stone-200">
       <Header />
-      <How />
-      <Example />
+      <Landing />
     </div>
   );
 }
