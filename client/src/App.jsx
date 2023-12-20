709be6afeb73ba103a517b5895562c93e3de0e64
Greg Pappas <gnpappas@proton.me>
2023-12-19 15:07:07 -1000

    Add Example component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import How from "./How";
-
+import Example from "./Example";
 function App() {
   return (
     <div id="main" className="h-[100dvh] text-stone-200">
       <Header />
       <How />
+      <Example />
     </div>
   );
 }
