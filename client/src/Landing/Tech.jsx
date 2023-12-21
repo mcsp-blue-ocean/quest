5255242849a47d75330958891bd3b08fadd009bc
Greg Pappas <gnpappas@proton.me>
2023-12-21 08:54:39 -1000

    Minor icon tweaks

--- client/src/Landing/Tech.jsx
+++ client/src/Landing/Tech.jsx
@@ -2,13 +2,14 @@ import { techIcons } from "./techData";
 
 const Tech = () => {
   return (
-    <ul id="tech-icons" className="flex">
+    <ul id="tech-icons" className="flex gap-3">
       {techIcons.map(({ id, image, alt }) => (
-        <li key={id} style={{ margin: ".25em"}}>
+        <li key={id}>
           <img
             src={image}
             alt={alt}
-            style={{ aspectRatio: "1/1", border: "1px solid #282524" }}
+            style={{ width: "100%", aspectRatio: "1/1" }}
+            className="mb-2"
           ></img>
         </li>
       ))}
