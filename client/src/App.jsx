be16119f753a5e170dd11f06d7e19490761ff684
Mark Northup <marknorthup@Marks-Air.attlocal.net>
2023-12-21 15:57:37 -0600

    changes have been made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import React, { useState } from "react";
 import Header from "./Header";
 import Landing from "./Landing";
+import Modal from "./Landing/Modal"; // Make sure to import the Modal component
+
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleToggleModal = () => {
+    setOpenModal(!openModal);
+  };
+
   return (
     <div id="main" className="h-[100dvh] text-stone-200">
+      <Header onToggleModal={handleToggleModal} />
+      <Modal open={openModal} />
       <Landing />
     </div>
   );
--- client/src/Landing.jsx
+++ client/src/Landing.jsx
@@ -2,18 +2,10 @@ import Example from "./Landing/Example";
 import How from "./Landing/How";
 import Modal from "./Landing/Modal";
 import React, { useState } from "react";
-import Header from "./Header";
 
 const Landing = () => {
-  const [openModal, setOpenModal] = useState(false);
-
-  const handleToggleModal = () => {
-    setOpenModal(!openModal);
-  };
   return (
     <>
-      <Header onToggleModal={handleToggleModal} />
-      <Modal open={openModal} />
       <How />
       <Example />
     </>
