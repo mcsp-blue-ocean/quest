02fe8f293237b4e6ae0d9521cca848a40daee6df
Greg Pappas <gnpappas@proton.me>
2023-12-21 07:01:06 -1000

    dotenv brough into vite config to run proxy correctly

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { imagetools } from "vite-imagetools";
-// https://vitejs.dev/config/
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
 export default defineConfig({
   plugins: [
     react(),
