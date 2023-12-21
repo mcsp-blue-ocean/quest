3d027ae79b5f3a7b2370d37fd8c2657e6f033114
Greg Pappas <gnpappas@proton.me>
2023-12-20 14:14:30 -1000

    Max width added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 import Landing from "./Landing";
 function App() {
   return (
-    <div id="main" className="h-[100dvh] text-stone-200">
+    <div id="main" className="mx-auto max-w-7xl h-[100dvh] text-stone-200">
       <Header />
       <Landing />
       <Footer />
