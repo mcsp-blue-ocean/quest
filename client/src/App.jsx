b0715b990e14480e0881a3e4d5a28ce16f9c05b8
Greg Pappas <gnpappas@proton.me>
2023-12-22 07:27:20 -1000

    Co-authored-by: Mitchell Keo <mitchellnkeo@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Landing from "./Landing";
-import Chatbot from "./Chatbot"
+import Chatbot from "./Chatbot";
 
 function App() {
   return (
     <div id="main" className="mx-auto max-w-7xl h-[100dvh] text-stone-200">
       <Header />
       <Landing />
-      <Footer />
       <Chatbot />
+      <Footer />
     </div>
   );
 }
--- client/src/Chatbot.jsx
+++ client/src/Chatbot.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './Chatbot.css';
 
 const Chatbot = () => {
   // State variables
