bf9815d735e4e780df1d248dfb115d44932b6db5
John Gordinier <jsgordinier@gmail.com>
2023-12-22 09:54:40 -0600

    trying to merge...again. Merger nightmare.

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,8 +1,8 @@
 import HeaderNav from "./Header/HeaderNav";
 import logo from "./assets/header/logo.png?web";
 import login from "./assets/header/login.svg";
+import React from "react";
 
-const Header = ({ onToggleModal }) => {
 const Header = ({ onToggleModal }) => {
   return (
     <div className="flex sm:flex-row flex-col items-center justify-between p-3 bg-sky-800 uppercase">
