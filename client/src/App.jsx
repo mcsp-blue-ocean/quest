58fb4fe16fca672aeaee165a6cefbe691549e0a6
Quinn Kaji <quinnkaji@gmail.com>
2023-12-22 10:28:34 -1000

    react router imported and working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import Footer from "./Footer";
 import Landing from "./Landing";
 import Modal from "./Modal";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   const [openModal, setOpenModal] = useState(false);
 
@@ -13,10 +15,17 @@ function App() {
 
   return (
     <div id="main" className="mx-auto max-w-7xl h-dvh text-stone-200">
-      <Header onToggleModal={handleToggleModal} />
-      {openModal && <Modal />}
-      <Landing />
+      <Router>
+        <Header onToggleModal={handleToggleModal} />
+
+        <Routes>
+          <Route path="/home" Component={Landing} />
+          <Route path="*" Component={Landing} />
+        </Routes>
+      </Router>
+
       <Footer />
+      {openModal && <Modal />}
     </div>
   );
 }
