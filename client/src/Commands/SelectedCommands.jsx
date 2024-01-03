4eb41b7b255f5988be41f5891244fd37351c6804
Greg Pappas <88697977+gnprwx@users.noreply.github.com>
2024-01-02 14:35:58 -1000

    UI/UX - Style commands (#133)
    * Style categories
    
    * Style commands

--- client/src/Commands/SelectedCommands.jsx
+++ client/src/Commands/SelectedCommands.jsx
@@ -1,38 +1,54 @@
+import { Link } from "react-router-dom";
+
 const SelectedCommands = ({
   selectedCategoryId,
   filteredCommands,
   categories,
 }) => {
-  console.log("hello again!");
-  console.log(filteredCommands[0], "filtered commands from command tester");
-  console.log(categories);
+  // console.log("hello again!");
+  // console.log(filteredCommands[0], "filtered commands from command tester");
+  // console.log(categories);
 
-  let categoryName;
-
-  // this is a terrible way to get the correct category name
-  TODO: for (let el of categories) {
-    if (el.id === selectedCategoryId) {
-      categoryName = el.category;
-    }
-  }
+  const categoryParent = categories.map(
+    (el) => el.id === selectedCategoryId && el.parent_category
+  );
+  const categoryName = categories.map(
+    (el) => el.id === selectedCategoryId && el.category
+  );
 
   return (
-    <>
-      {categoryName && <h2>{categoryName}</h2>}
-      <ul style={{ backgroundColor: "black" }}>
+    <div className="md:w-3/4 mx-auto flex flex-col items-center bg-slate-300 pb-10 my-4 rounded relative">
+      <p className="bg-sky-900 rounded inline font-bold p-1 m-1 absolute top-0 left-0">
+        {categoryParent}
+      </p>
+      <Link
+        to="/categories"
+        className="text-black text-2xl font-bold absolute top-0 right-0 px-2"
+      >
+        x
+      </Link>
+      <p className="text-center text-black font-bold underline text-5xl mt-10">
+        {categoryName}
+      </p>
+      <ul className="md:w-3/4 mx-auto flex flex-col gap-6 bg-sky-900 mt-10 rounded sm:p-6 p-2 mx-1">
         {filteredCommands &&
           filteredCommands.map((command, index) => (
             <li
               key={index}
-              style={{ margin: ".25em", height: "5em", width: "100%" }}
+              className="bg-sky-950 sm:p-4 p-2 rounded shadow text-lg"
             >
-              <p>Syntax: {command.command_syntax} </p>
-              <p>Description:{command.command_description}</p>
-              <br />
+              <p>
+                <span className="font-bold">Syntax: </span>
+                {command.command_syntax}
+              </p>
+              <p>
+                <span className="font-bold">Description: </span>
+                {command.command_description}
+              </p>
             </li>
           ))}
       </ul>
-    </>
+    </div>
   );
 };
 
