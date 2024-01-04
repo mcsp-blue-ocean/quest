2e51cfc247d1a8449c61b81f25332d13b5fe01b0
John Gordinier <jsgordinier@gmail.com>
2024-01-04 12:24:08 -0600

    Updated with three tests on CRUD

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,19 +6,6 @@ import { Link } from "react-router-dom";
 
 const Header = ({ onToggleModal }) => {
   return (
-<<<<<<< HEAD
-    <div className="flex sm:flex-row flex-col items-center justify-between p-3 bg-sky-800 uppercase">
-      <HeaderNav />
-      <img src={logo} className="w-28 order-first sm:order-none" />
-      <Link to="/admin">
-        <img
-          src={login}
-          alt="login"
-          className="w-14 cursor-pointer"
-          onClick={onToggleModal}
-        />
-      </Link>
-=======
     <div className="bg-sky-800">
       <div className="mx-auto lg:w-3/4 flex flex-row items-center justify-between uppercase p-3 ">
         <HeaderNav />
@@ -32,7 +19,6 @@ const Header = ({ onToggleModal }) => {
           />
         </Link>
       </div>
->>>>>>> a01ddae0ebe402c3fff5a4a61efb8e19d3c4224b
     </div>
   );
 };
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,4 +1,5 @@
 import { it, expect, beforeEach, afterEach } from "vitest";
+//TESTS ARE PERFORMED ON A TEST DATABASE
 process.env.DATABASE_URL = "postgres://localhost/quest_test";
 import { app, db } from "../server/server.js";
 import request from "supertest";
@@ -70,31 +71,46 @@ it("GET /api/categories", async () => {
 
 //
 //
-//TEST FOR COMMANDS BY CATEGORY
+// TEST FOR COMMANDS BY CATEGORY
+beforeEach(async () => {
+  await db.query("DELETE FROM categories");
+});
 
-// it("GET /api/commands/:categoryId returns an array of commands for a specific category", async () => {
-//   const categoryId = 5;
+it("GET /api/commands/:categoryId", async () => {
+  // Create a category
+  await db.query(
+    "INSERT INTO categories (id, category, parent_category) VALUES (3, 'TestCategory', 'ParentCategory')"
+  );
 
-//   await db.query(
-//     "INSERT INTO commands (category_id, command_syntax, command_description) VALUES(?, 'npm run test', 'Runs the tests for the project')",
-//     [categoryId]
-//   );
+  // Insert commands for the created category
+  await db.query(
+    "INSERT INTO commands (category_id, command_syntax, command_description) VALUES(3, 'npm run test', 'Runs the tests for the project')"
+  );
+  await db.query(
+    "INSERT INTO commands (category_id, command_syntax, command_description) VALUES(3, 'npm run test two', 'Runs the tests for the project two')"
+  );
+  await db.query(
+    "INSERT INTO commands (category_id, command_syntax, command_description) VALUES(3, 'npm run test three', 'Runs the tests for the project three')"
+  );
 
-//   const res = await request(app).get(`/api/commands/${categoryId}`);
-//   expect(res.status).toBe(200);
-//   expect(Array.isArray(res.body)).toBe(true);
+  // Perform GET request for the specific category
+  const { status, body } = await request(app).get("/api/commands/3");
+  expect(status).toBe(200);
 
-//   // Add more specific assertions based on your expected response format
-//   // For example, check if the first command in the array has the expected properties
-//   if (res.body.length > 0) {
-//     const [firstCommand] = res.body;
-//     expect(firstCommand).toMatchObject({
-//       category_id: categoryId,
-//       command_syntax: "npm run test",
-//       command_description: "Runs the tests for the project",
-//     });
-//   }
-// });
+  expect(body).toBeInstanceOf(Array);
+  expect(body).toHaveLength(3);
+
+  const commandSyntaxes = body.map((command) => command.command_syntax);
+  expect(commandSyntaxes).toContain("npm run test");
+  expect(commandSyntaxes).toContain("npm run test two");
+  expect(commandSyntaxes).toContain("npm run test three");
+
+  // Clear data
+  afterEach(async () => {
+    await db.query("DELETE FROM commands");
+    await db.query("DELETE FROM categories");
+  });
+});
 
 // //   // Test for admin login
 // //   test("POST /api/login returns a token for valid admin credentials", async () => {
